827c60ae0abd417afdee156f5dcbc80e5c7a1691
Cong Nguyen <kaom.n.92@gmail.com>
2022-02-11 14:32:43 +0200

    working line drawing Algorithm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: conguyen <conguyen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 11:11:50 by conguyen          #+#    #+#             */
-/*   Updated: 2022/02/11 11:22:51 by conguyen         ###   ########.fr       */
+/*   Updated: 2022/02/11 14:32:29 by conguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -168,39 +168,39 @@ void	render_image(int **int_array, int height, int length)
 			// 		break ;
 		}
 	}
-	for (int x = 0; x < height; x++)
-	{
-		for (int y = 0; y < length; y++)
-		{
-			pixel.x1 = (y * 35 + 450) - (x * 40);
-			pixel.y1 = (x * 35 + 50) + (y * 20);
-			pixel.x2 = ((y + 1) * 35 + 450) - ((x) * 40);
-			pixel.y2 = ((x) * 35 + 50) + ((y + 1) * 20);
-			pixel.dx = abs(pixel.x2 - pixel.x1);
-			pixel.dy = abs(pixel.y2 - pixel.y1);
-			//printf("start = %d,%d, end = %d,%d\n", pixel.x1, pixel.y1, pixel.x2, pixel.y2);
-			//exit (0);
-			if (int_array[x][y] == 10 && int_array[x][y + 1] == 10)
-			{
-				if (pixel.dx > pixel.dy)
-					draw_line(data, pixel.x1, pixel.y1, pixel.x2, pixel.y2, 0, pixel.dx, pixel.dy, 0x00FF0000);
-				else
-					draw_line(data, pixel.y1, pixel.x1, pixel.y2, pixel.x2, 1, pixel.dy, pixel.dx, 0x00FF0000);
-			}
-			else
-			//my_mlx_pixel_put(&data, (y * 35 + 450) - (x * 40), (x * 35 + 50) + (y * 20), 0x00FFFFFF);
-			// if (y == 2)
-			// 	break ;
-			{
-				if (pixel.dx > pixel.dy)
-					draw_line(data, pixel.x1, pixel.y1, pixel.x2, pixel.y2, 0, pixel.dx, pixel.dy, 0x00FFFFFF);
-				else
-					draw_line(data, pixel.y1, pixel.x1, pixel.y2, pixel.x2, 1, pixel.dy, pixel.dx, 0x00FFFFFF);
-			}
-			// if (y == 2)
-			// 		break ;
-		}
-	}
+	// for (int x = 0; x < height; x++)
+	// {
+	// 	for (int y = 0; y < length; y++)
+	// 	{
+	// 		pixel.x1 = (y * 35 + 450) - (x * 40);
+	// 		pixel.y1 = (x * 35 + 50) + (y * 20);
+	// 		pixel.x2 = ((y + 1) * 35 + 450) - ((x) * 40);
+	// 		pixel.y2 = ((x) * 35 + 50) + ((y + 1) * 20);
+	// 		pixel.dx = abs(pixel.x2 - pixel.x1);
+	// 		pixel.dy = abs(pixel.y2 - pixel.y1);
+	// 		//printf("start = %d,%d, end = %d,%d\n", pixel.x1, pixel.y1, pixel.x2, pixel.y2);
+	// 		//exit (0);
+	// 		if (int_array[x][y] == 10 && int_array[x][y + 1] == 10)
+	// 		{
+	// 			if (pixel.dx > pixel.dy)
+	// 				draw_line(data, pixel.x1, pixel.y1, pixel.x2, pixel.y2, 0, pixel.dx, pixel.dy, 0x00FF0000);
+	// 			else
+	// 				draw_line(data, pixel.y1, pixel.x1, pixel.y2, pixel.x2, 1, pixel.dy, pixel.dx, 0x00FF0000);
+	// 		}
+	// 		else
+	// 		//my_mlx_pixel_put(&data, (y * 35 + 450) - (x * 40), (x * 35 + 50) + (y * 20), 0x00FFFFFF);
+	// 		// if (y == 2)
+	// 		// 	break ;
+	// 		{
+	// 			if (pixel.dx > pixel.dy)
+	// 				draw_line(data, pixel.x1, pixel.y1, pixel.x2, pixel.y2, 0, pixel.dx, pixel.dy, 0x00FFFFFF);
+	// 			else
+	// 				draw_line(data, pixel.y1, pixel.x1, pixel.y2, pixel.x2, 1, pixel.dy, pixel.dx, 0x00FFFFFF);
+	// 		}
+	// 		// if (y == 2)
+	// 		// 		break ;
+	// 	}
+	// }
 	mlx_put_image_to_window(mlx, win, data.img, 0, 0);
 	mlx_key_hook(win, &esc_key, 0);
 	mlx_loop(mlx);
